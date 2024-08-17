fb2ba81ce01f7713b875e655865e804a952d5892
ans16086 <bestpapamama12@gmail.com>
2024-08-17 15:58:52 +0500

    a

--- enviroment/core/userprof/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class UserprofConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'userprof'
--- enviroment/core/userprof/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from django.views.generic import TemplateView
-
-app_name = 'userprof'
-urlpatterns = [
-path('',TemplateView.as_view(template_name = "userprof.html"))
-]
